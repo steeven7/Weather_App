c1f4d41dc4d86d2e06bf8c59b83c7599ce76c72d
steeven7 <steeven8130@gmail.com>
2020-08-26 14:28:31 +0100

    App using OpenWeather api

---
+++ app.js
@@ -0,0 +1,55 @@
+//Get elements
+const weather = document.querySelector("#desc");
+const temp = document.querySelector(".temp");
+const search = document.querySelector(".search input");
+const country = document.querySelector(".country");
+const icon = document.querySelector(".weather-icon");
+const city = document.querySelector(".city")
+const date = document.querySelector(".date"); 
+const forecast = {};
+
+//Get city value
+search.addEventListener("keypress", searchCity);
+function searchCity(e) {
+	if (e.key === "Enter") {
+		const location = search.value;
+		getCity(location);
+	}
+}
+
+//Insert city value into api
+function getCity(location) {
+	const key = "41a33c1f6739002732956da85ecbd727";
+	const url = `http://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${key}`;
+	if (location === "") alert("enter a city");
+
+	axios
+		.get(`${url}`)
+		.then(({ data }) => {
+			console.log(data);
+			getData(data);
+		})
+		.catch(function (error) {
+			console.log(error);
+		})
+		.then(() => getWeather());
+
+	//Get api Data
+	function getData(data) {
+		forecast.temperature = Math.floor(data.main.temp);
+		forecast.description = data.weather[0].description;
+		forecast.city = data.name;
+		forecast.country = data.sys.country;
+		forecast.image = data.weather[0].icon;
+	}
+
+	//Insert data into UI
+	function getWeather() {
+		city.innerHTML = `${forecast.city},` + `${forecast.country}`;
+		weather.innerHTML = `${forecast.description}`;
+		temp.innerHTML = `Current Temp:${forecast.temperature}&degC`;
+		icon.innerHTML = `<img src="icons/${forecast.image}.png">`;
+
+		search.value = '';
+	}
+}
